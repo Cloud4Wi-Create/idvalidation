a65c6c5200fe836f4249d7c28550332058f97805
Ivan Muccini <muccini.ivan@gmail.com>
2020-05-25 21:41:32 -0700

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var got = require('got');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/validate', (req, res) => {
-  
-console.log(req.body);
 
+
+router.post('/validate', function (req, res) {
+  
+  var ticket = req.body.ticket;
+  console.log("ticket");
+
+
+  body = 'valid';
+  res.status('200').send('body')
 
 });
 
