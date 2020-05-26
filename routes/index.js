174faaf672ba3ff03165467206a5a78e615f3fed
Ivan Muccini <muccini.ivan@gmail.com>
2020-05-26 10:45:30 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var got = require('got');
+
+const https = require('https');
+const request = require('request');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,13 +13,47 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/validate', function (req, res) {
-  
   var ticket = req.body.ticket;
   console.log("ticket");
 
 
-  body = 'valid';
-  res.status('200').send('body')
+/*
+  httpurl = 'http://www.mocky.io/v2/5ecca0603200002a00236205';
+
+  request.post({
+    url: httpurl,
+    json: {
+      ticket: ticket,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST'
+  }, (error, response, body) => {
+        if(error) {
+          console.log("errore: "+error);
+          console.log(`statusCode: ${response.statusCode}`)
+            res.status(404).send(JSON.parse(error))
+        }
+        console.log("Tciket: " +JSON.stringify(body));
+        var statuscode = response.statusCode;
+        console.log("Status code: " +statuscode);
+        console.log("Body: " +JSON.stringify(body.status));
+        res.status(statuscode).send(body)
+    });
+    */
+
+    
+    // mock response for demo only
+    if (ticket=="0603200002") {
+      body = {"status":"valid"};
+      res.status('200').send(body)
+    }
+    else {
+      body = {"status":"invalid"};
+      res.status('200').send(body)
+    }
+
 
 });
 
